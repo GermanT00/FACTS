ae6630f4b8099a0d2a3ce113cbad117db50df18b
GermanT00 <trianogerman@gmail.com>
2023-10-28 17:31:13 -0300

    FACTS STREET WEAR

---
+++ src/componentes/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './navBar'; // Ruta actualizada
+import './styles.css'; // Ruta actualizada
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Contenido de la tienda digital</h1>
+    </div>
+  );
+}
+
+export default App;
